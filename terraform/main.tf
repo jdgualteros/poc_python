f5001e879f253819ab763d3bd4a033be9ec80426
jimmy Gualteros <kimmydaniel.gualteros.contractor@bbva.com>
2022-09-04 18:12:45 -0500

    updated git action terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,7 +65,7 @@ resource "aws_db_instance" "pocpython" {
   engine                 = "postgres"
   engine_version         = "13"
   username               = "dbadmin"
-  password               = var.db_password
+  password               = "admin123456"
   db_subnet_group_name   = aws_db_subnet_group.pocpython.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.pocpython.name
