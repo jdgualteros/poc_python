8212961958da6c034b11e970ea96e020cb554127
jimmy Gualteros <kimmydaniel.gualteros.contractor@bbva.com>
2022-09-04 18:01:37 -0500

    updated schemas validate

--- app/views/v_uploads.py
+++ app/views/v_uploads.py
@@ -9,23 +9,39 @@ import numpy as np
 
 router = APIRouter()
 
+
 def check_int(num):
     try:
         int(num)
     except ValueError:
         return False
     return True
-    
-int_validation = [CustomElementValidation(lambda i: check_int(i), lambda i: print(i))]
-null_validation = [CustomElementValidation(lambda d: d is not np.nan, lambda i: print(i))]
 
-schema = Schema([
+
+int_validation = [CustomElementValidation(
+    lambda i: check_int(i), lambda i: print(i))]
+null_validation = [CustomElementValidation(
+    lambda d: d is not np.nan, lambda i: print(i))]
+
+sch_employees = Schema([
     Column('id', null_validation + int_validation),
     Column('name', null_validation),
     Column('datetime', null_validation),
     Column('department_id', null_validation + int_validation),
     Column('job_id', null_validation + int_validation)
 ])
+
+
+sch_jobs = Schema([
+        Column('id', null_validation + int_validation),
+        Column('job', null_validation)
+    ])
+
+sch_departments = Schema([
+    Column('id', null_validation + int_validation),
+    Column('department', null_validation)
+])
+
 
 
 @router.post("/upload/{table}", tags=["Upload Files"])
@@ -35,10 +51,12 @@ async def create_upload_file(table, file: UploadFile = File(...)):
     data = StringIO(s)
     df = pd.read_csv(data, names=name_columns_csv[table], sep=',')
     # out = clear_data(df)
-    errors = schema.validate(df)
+    errors = ('sch_' + table).validate(df)
     errors_index_rows = [e.row for e in errors]
     data_clear = df.drop(index=errors_index_rows)
-    data_clear.to_sql(table, engine, if_exists='append', index=False, chunksize=1000, dtype=df_schema[table])
+    data_clear.to_sql(table, engine, if_exists='append',
+                      index=False, chunksize=1000, dtype=df_schema[table])
     errors_rows = df.filter(items=errors_index_rows, axis=0)
-    errors_rows.to_sql(table + '_errors', engine, if_exists='append', index=False, chunksize=1000, dtype=df_schema[table])
+    errors_rows.to_sql(table + '_errors', engine, if_exists='append',
+                       index=False, chunksize=1000, dtype=df_schema[table])
     return {file.filename}
